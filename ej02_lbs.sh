cf75b831e3a042df748e2a74968bb4ed743518f5
lucia-blanco <lublasal@gmail.com>
2018-05-26 22:49:59 +0200

    this fffffffreaking one

--- ej02_lbs.sh
+++ ej02_lbs.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-if test $1 -le 0; then
-	echo "error"
-else
+if [ "$1" -ge 0 -a -z "$2" ]; then
 	i=1
-	while [ $i -le $1 ]; do
-		touch file_$i.txt
+        while [ $i -le $1 ]; do
+                touch "file_0$i.txt"
 		((i++))
-	done
+        done
+        echo "done"
+elif [ "$1" -ge 0 -a -n "$2" ]; then
+	i=1
+        if [ -d "$2" ]; then
+		cd "$2"
+        	while [ $i -le $1 ]; do
+                	touch "file_$i.txt"
+			((i++))
+		        echo "done"
+        	done
+        else
+	        mkdir "$2"
+        	cd "$2"
+        	while [ $i -le $1 ]; do
+                	touch "file_$i.txt"
+			((i++))
+		        echo "done"
+		done
+	fi
+else 
+		echo "Error"
 fi
 
